b5b07765fea85bb9e9fddfbc97e5f51df10463d9
Administrator <wenyefbl@163.com>
2017-09-19 16:04:04 +0800

    Rejected commit: Default

--- src/main/java/com/com/util/GetTextLines.java
+++ src/main/java/com/com/util/GetTextLines.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 
-class GetTextLines {
+public class GetTextLines {
     int getTextLines(String path) {
         int x = -1;
         try {
